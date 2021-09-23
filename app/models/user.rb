72b499542ad36f8767be575241515973699fcc9f
Gokularangan <rangangokul0@gmail.com>
2021-09-23 08:03:27 +0000

    Finish toy App

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates FILE_IN, presence: true  # Replace FILE_IN with the right code.
-  validates FILE_IN, presence: true  # Replace FIlE_IN with the right code.
+  validates FILL_IN, presence: true  
+  validates FILL_IN, presence: true  
 end
